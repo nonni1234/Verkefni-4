8aa3c20244d4773d7485924f94687c72c0b673f4
nonni1234 <nonni1234567@gmail.com>
2020-04-05 13:36:13 +0000

    ætti að vera komið

---
+++ Assets/PickUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Debug.Log("Picked up");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizMove = 0f;
+    float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    // Start is called before the first frame update
+    public void OnLanding() {
+        animator.SetBool("isJumping", false);
+    }
+    public void OnCrouching(bool isCrouching) {
+        animator.SetBool("isCrouching", isCrouching);
+    }
+    void Update() { // Fá movement input
+        horizMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
+        }
+        animator.SetFloat("Speed", Mathf.Abs(horizMove));
+    }
+
+    void FixedUpdate() { // Hreyfa Character
+        controller.Move(horizMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
